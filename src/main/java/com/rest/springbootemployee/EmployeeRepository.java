d64e1245ebbd2c950200afd212ffeb75b2ebe1e3
1643953546 <1643953546@qq.com>
2022-07-26 15:51:15 +0800

    feat:[smith huang] add GET employees api

---
+++ src/main/java/com/rest/springbootemployee/EmployeeRepository.java
@@ -0,0 +1,22 @@
+package com.rest.springbootemployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeRepository {
+    private List<Employee> employees=new ArrayList<>();
+
+    public EmployeeRepository(){
+        employees.add(new Employee(1,"huang",23,"male",8000));
+        employees.add(new Employee(2,"zhang",23,"male",8000));
+        employees.add(new Employee(3,"li",23,"female",8000));
+        employees.add(new Employee(4,"qian",23,"male",7989));
+        employees.add(new Employee(5,"xing",23,"female",9000));
+    }
+
+    public List<Employee> findAll() {
+        return employees;
+    }
+}
