ddde8e7ccc75c4733cc7eaf911bb942a03609d79
WUWI11 <wuwi11@oocl.com>
2022-07-27 14:09:25 +0800

    test: implement test GET /companies/1/employees API

--- src/test/java/com/rest/springbootemployee/controller/CompanyControllerTest.java
+++ src/test/java/com/rest/springbootemployee/controller/CompanyControllerTest.java
@@ -66,4 +66,19 @@ public class CompanyControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.employees[0].salary").value(2532));
 
     }
+
+    @Test
+    void should_get_employees_under_certain_company_when_perform_get_given_id() throws Exception {
+        //given
+        companyRepository.createCompany(new Company(1, "huawei", new ArrayList<>(Arrays.asList(new Employee(1, "zhangsan", 12, "male", 2532)))));
+        int id = 1;
+        //when & then
+        client.perform(MockMvcRequestBuilders.get("/companies/" + id + "/employees"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("zhangsan"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(12))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].gender").value("male"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].salary").value(2532));
+
+    }
 }
