684c81b4dc6e8ceedf3baed37cb36ed5bca1dfc8
WUWI11 <wuwi11@oocl.com>
2022-07-29 08:43:37 +0800

    refactor: integrate test change to h2

--- src/test/java/com/rest/springbootemployee/controller/CompanyControllerTest.java
+++ src/test/java/com/rest/springbootemployee/controller/CompanyControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -26,6 +27,7 @@ import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class CompanyControllerTest {
 
     @Autowired
